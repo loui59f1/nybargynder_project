2168211e8461ca85a442637f58ab3e87c9ca8bfd
loui59f1 <loui59f1@stud.kea.dk>
2020-09-15 10:03:26 +0200

    Kommentarer på JS og JSON - til teknisk dokumentation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+// Definere variabler
 let menu;
 let modal = document.querySelector("#modal");
 let filter = "alle";
 const url = "https://spreadsheets.google.com/feeds/list/1cAqkDsR47rgBQSuukBgDbAvSEqFL5-HIcXBbo4SgzDw/od6/public/values?alt=json";
 
-
+// Venter på alt indhold er loadet --> funktion loadJSON
 document.addEventListener("DOMContentLoaded", loadJSON);
 
+// Funktion der henter indhold/data fra Google Sheet --> visMenu
 async function loadJSON() {
 	const JSONData = await fetch(url);
 	menu = await JSONData.json();
@@ -13,6 +15,7 @@ async function loadJSON() {
 	visMenu();
 }
 
+// Funktion der sætter data fra Google Sheet ind i #liste (list variabel) template
 function visMenu() {
 	const list = document.querySelector("#liste");
 	const template = document.querySelector("template");
@@ -27,12 +30,15 @@ function visMenu() {
 			clone.querySelector("img").src = "img/" + drink.gsx$image.$t + ".jpg";
 			clone.querySelector(".kort").textContent = drink.gsx$short.$t;
 			clone.querySelector(".oprindelse").textContent = "Oprindelse: " + drink.gsx$origin.$t;
+
+			// Når man klikker på article/drink vil funktionen visDetaljer (modal/popup) kaldes
 			clone.querySelector("article").addEventListener("click", () => visDetaljer(drink));
 			list.appendChild(clone);
 		}
 	})
 }
 
+// Denne funktion sætter vores drink info ind i den klargjorte modal sektion
 function visDetaljer(drink) {
 	modal.style.display = "block";
 	modal.querySelector("h2").textContent = drink.gsx$name.$t;
@@ -41,21 +47,24 @@ function visDetaljer(drink) {
 	modal.querySelector("img").src = "img/" + drink.gsx$image.$t + ".jpg";
 }
 
+// Ved klik på krydset lukkes popup/modal
 document.querySelector("#close").addEventListener("click", () => modal.style.display = "none");
 
-// Ved klik udenfor modal lukkes den
+// Ved klik udenfor modal lukkes den også - kode fundet online.
 window.onclick = function (event) {
 	if (event.target == modal) {
 		modal.style.display = "none";
 	}
 }
 
+// Ved klik på alle filter knapper, kaldes filterBtns funktionen
 function addEventListenersToButtons() {
 	document.querySelectorAll(".filter").forEach((btn) => {
 		btn.addEventListener("click", filterBtns);
 	})
 }
 
+// Denne funktion filtrere på indholdet ved at kigge på dataset og holde op mod kategorien på drinks
 function filterBtns() {
 	filter = this.dataset.kategori;
 	document.querySelector(".heading").textContent = this.textContent;
