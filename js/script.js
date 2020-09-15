1e25d50cde15caaad5e92a270a515d4726e28fb0
Morten Gross <morten@Mortens-Air.domain_not_set.invalid>
2020-09-15 13:53:07 +0200

    Ændring af .product HTML opbygning, samt .text_color tilføjet i forhold til .oprindelse

--- js/script.js
+++ js/script.js
@@ -9,42 +9,42 @@ document.addEventListener("DOMContentLoaded", loadJSON);
 
 // Funktion der henter indhold/data fra Google Sheet --> visMenu
 async function loadJSON() {
-	const JSONData = await fetch(url);
-	menu = await JSONData.json();
-	addEventListenersToButtons();
-	visMenu();
+    const JSONData = await fetch(url);
+    menu = await JSONData.json();
+    addEventListenersToButtons();
+    visMenu();
 }
 
 // Funktion der sætter data fra Google Sheet ind i #liste (list variabel) template
 function visMenu() {
-	const list = document.querySelector("#liste");
-	const template = document.querySelector("template");
-	// Tømmer sektion for indhold
-	list.innerHTML = "";
-	// Loop igennem indhold og filtrerer
-	menu.feed.entry.forEach(drink => {
-		if (filter == "alle" || filter == drink.gsx$kategori.$t) {
-			console.log(drink);
-			const clone = template.cloneNode(true).content;
-			clone.querySelector("h2").textContent = drink.gsx$name.$t;
-			clone.querySelector("img").src = "img/" + drink.gsx$image.$t + ".jpg";
-			clone.querySelector(".kort").textContent = drink.gsx$short.$t;
-			clone.querySelector(".oprindelse").textContent = "Oprindelse: " + drink.gsx$origin.$t;
+    const list = document.querySelector("#liste");
+    const template = document.querySelector("template");
+    // Tømmer sektion for indhold
+    list.innerHTML = "";
+    // Loop igennem indhold og filtrerer
+    menu.feed.entry.forEach(drink => {
+        if (filter == "alle" || filter == drink.gsx$kategori.$t) {
+            console.log(drink);
+            const clone = template.cloneNode(true).content;
+            clone.querySelector("h2").textContent = drink.gsx$name.$t;
+            clone.querySelector("img").src = "img/" + drink.gsx$image.$t + ".jpg";
+            clone.querySelector(".kort").textContent = drink.gsx$short.$t;
+            clone.querySelector(".oprindelse").textContent = drink.gsx$origin.$t;
 
-			// Når man klikker på article/drink vil funktionen visDetaljer (modal/popup) kaldes
-			clone.querySelector("article").addEventListener("click", () => visDetaljer(drink));
-			list.appendChild(clone);
-		}
-	})
+            // Når man klikker på article/drink vil funktionen visDetaljer (modal/popup) kaldes
+            clone.querySelector("article").addEventListener("click", () => visDetaljer(drink));
+            list.appendChild(clone);
+        }
+    })
 }
 
 // Denne funktion sætter vores drink info ind i den klargjorte modal sektion
 function visDetaljer(drink) {
-	modal.style.display = "block";
-	modal.querySelector("h2").textContent = drink.gsx$name.$t;
-	modal.querySelector(".lang").textContent = drink.gsx$long.$t;
-	modal.querySelector(".oprindelse").textContent = drink.gsx$origin.$t;
-	modal.querySelector("img").src = "img/" + drink.gsx$image.$t + ".jpg";
+    modal.style.display = "block";
+    modal.querySelector("h2").textContent = drink.gsx$name.$t;
+    modal.querySelector(".lang").textContent = drink.gsx$long.$t;
+    modal.querySelector(".oprindelse").textContent = drink.gsx$origin.$t;
+    modal.querySelector("img").src = "img/" + drink.gsx$image.$t + ".jpg";
 }
 
 // Ved klik på krydset lukkes popup/modal
@@ -52,25 +52,25 @@ document.querySelector("#close").addEventListener("click", () => modal.style.dis
 
 // Ved klik udenfor modal lukkes den også - kode fundet online.
 window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 // Ved klik på alle filter knapper, kaldes filterBtns funktionen
 function addEventListenersToButtons() {
-	document.querySelectorAll(".filter").forEach((btn) => {
-		btn.addEventListener("click", filterBtns);
-	})
+    document.querySelectorAll(".filter").forEach((btn) => {
+        btn.addEventListener("click", filterBtns);
+    })
 }
 
 // Denne funktion filtrere på indholdet ved at kigge på dataset og holde op mod kategorien på drinks
 function filterBtns() {
-	filter = this.dataset.kategori;
-	document.querySelector(".heading").textContent = this.textContent;
-	document.querySelectorAll(".filter").forEach((btn) => {
-		btn.classList.remove("active");
-	})
-	this.classList.add("active");
-	visMenu();
+    filter = this.dataset.kategori;
+    document.querySelector(".heading").textContent = this.textContent;
+    document.querySelectorAll(".filter").forEach((btn) => {
+        btn.classList.remove("active");
+    })
+    this.classList.add("active");
+    visMenu();
 }
